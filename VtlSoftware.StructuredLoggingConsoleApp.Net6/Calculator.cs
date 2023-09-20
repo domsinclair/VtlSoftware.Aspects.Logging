afd3573d58b45f4c5dc02309626922c132251c1e
Dom Sinclair <dom@viewtolearn.co.uk>
2023-09-20 12:08:33 +0100

    Added Structured Logging Demo

---
+++ VtlSoftware.StructuredLoggingConsoleApp.Net6/Calculator.cs
@@ -0,0 +1,39 @@
+﻿using VtlSoftware.Aspects.Logging.Net6;
+
+namespace VtlSoftware.StructuredLoggingConsoleApp.Net6
+{
+    internal class Calculator
+    {
+        #region Constructors
+
+        public Calculator()
+        {
+        }
+
+        #endregion
+
+        #region Public Methods
+        [Log]
+        public double Add(double a, double b) { return a + b; }
+
+        [Log]
+        public double Divide(double a, double b) { return a / b; }
+
+        [Log]
+        public double Multiply(double a, double b) { return a * b; }
+
+        [Log]
+        public void Run(double a, double b)
+        {
+            Add(a, b);
+            Subtract(a, b);
+            Multiply(a, b);
+            Divide(a, b);
+        }
+
+        [Log]
+        public double Subtract(double a, double b) { return a - b; }
+
+        #endregion
+    }
+}
---
+++ VtlSoftware.StructuredLoggingConsoleApp.Net6/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace VtlSoftware.StructuredLoggingConsoleApp.Net6
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile(
+                $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json",
+                optional: true)
+            .AddEnvironmentVariables();
+        }
+
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Build())
+                .CreateLogger();
+
+            Log.Logger.Information("Application Startup");
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices(
+                    (context, services) =>
+                    {
+                        services.AddTransient<Calculator>();
+                    })
+                .UseSerilog()
+                .Build();
+
+            Calculator calculator = new Calculator();
+            calculator.Run(1, 4);
+
+            Log.CloseAndFlush();
+        }
+
+        #endregion
+    }
+}
